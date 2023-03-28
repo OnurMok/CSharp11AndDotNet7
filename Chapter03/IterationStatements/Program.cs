772eb16a2887f4fea4592cf07e435b203a812a42
OnurMok <onurmok@gmail.com>
2023-03-28 20:53:55 +0200

    3.28

---
+++ Chapter03/IterationStatements/Program.cs
@@ -0,0 +1,29 @@
+﻿
+int x = 0;
+while (x < 10)
+{
+    Console.WriteLine(x);
+    x++;
+}
+
+string? password;
+
+do
+{
+    Write("Enter your password: ");
+    password = Console.ReadLine();
+}
+while (password.Length < 8 );
+Console.WriteLine("Correct!");
+
+for (int i = 0; i < 10; i++)
+{
+    WriteLine(i);
+}
+
+string[] names = { "Adam", "Barry", "Charlie" };
+
+foreach (string name in names)
+{
+    WriteLine($"{name} has {name.Length} characters.");
+}
